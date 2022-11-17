25552f2c32ec201775e22cff7c37eac226a005ac
JuliusArtGallery <118159911+JuliusArtGallery@users.noreply.github.com>
2022-11-17 16:52:13 -0500

    Add files via upload

--- button.js
+++ button.js
@@ -1,7 +1,3 @@
-var btn = {
-    style: "main"
-};
-
 document.getElementById("btn").addEventListener("click", changeCSS);
 
 function changeCSS() {
@@ -10,10 +6,8 @@ function changeCSS() {
 
     if(style.getAttribute("href") == "main.css") {
         style.setAttribute("href", "alternate.css");
-        btn.style = "alternate";
     }
     else {
         style.setAttribute("href", "main.css");
-        btn.style = "main";
     }
 }
